6496944763a00f471b66ff7465a55a7a54f1b12d
AnikaCeamin22 <155265333+AnikaCeamin22@users.noreply.github.com>
2025-04-26 02:16:54 +0600

    Add files via upload

--- Bsics/fetch.js
+++ Bsics/fetch.js
@@ -1,36 +1,21 @@
-// async function getAll(){
-//     try {
-//         const response = await fetch('https://randomuser.me/api/')  //string
-//          const data = await response.json(); //convert string to json
-//          console.log(data);                
-//     } catch (error) {
-//         console.log("Error: =  ",error);        
-//     }
-// }
-// getAll();
+// Fetch API
+let file = "./Fetchdata.txt";
 
-//or
-fetch('https://randomuser.me/api/')
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data);    
-})
-.catch(function(error){
-    console.log("Error: =  ",error);
-})
+const find = () =>{
+    fetch(file)
+          .then((res)=>{
+            return res.text();
+          })
+          .then((res)=>{
+            document.getElementById('id1').innerHTML=res;
+          })
+}
 
-/*
-The fetch() method takes one mandatory argument, 
-the path to the resource you want to fetch.
+async function  find2(){
+    let res = await fetch(file);
+    let ans = await res.text();
+    document.getElementById('id2').innerHTML=ans;
+}
 
- It returns a Promise that resolves to the 
- Response to that request — as soon as 
- the server responds with headers —
- even if the server response is an HTTP error status.
-
-You can also optionally pass in an init options
- object as the second argument.
- */
+//
 
